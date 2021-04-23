f256725c2b4c9dcc37b6ec295dd9ab5b5b648da4
Drakx116 <alexandre2.guyot@gmail.com>
2021-04-23 13:35:33 +0200

    Newsletter - Init form

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\NewsletterUser;
+use App\Form\NewsletterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +14,18 @@ class NewsletterController extends AbstractController
     /**
      * @Route("/newsletter", name="newsletter")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('newsletter/index.twig', []);
+        $user = new NewsletterUser();
+        $form = $this->createForm(NewsletterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO : Handle form
+        }
+
+        return $this->render('newsletter/index.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
