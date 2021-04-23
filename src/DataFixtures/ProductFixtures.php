9e2f49ca325e26dd5a2bae160fcc4eae18d30231
Drakx116 <alexandre2.guyot@gmail.com>
2021-04-23 09:48:16 +0200

    ProductFixtures - Update product picture

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -16,7 +16,7 @@ class ProductFixtures extends Fixture
         $product = new Product();
         $product->setName('Doom Eternal - The Ancient Gods Part II');
         $product->setDescription('DOOM Eternal : The Ancient Gods, Part II reprend là où la première extension s’était arrêtée. Ce DLC entend bien donner une conclusion satisfaisante aux aventures du Doom Slayer. Si la première partie n’était pas exempte de défauts, elle proposait tout de même une expérience plaisante. Pour clore cette épopée, id Software semble avoir mis les petits plats dans les grands, car ce deuxième volet s’affranchit des griefs de son prédécesseur.');
-        $product->setPicture('https://image.jeuxvideo.com/medias/161660/1616598029-8610-capture-d-ecran.jpg');
+        $product->setPicture('https://new-game-plus.fr/wp-content/uploads/2021/03/DOOM-eternal-the-ancient-gods-part-2-screen-scaled.jpg');
 
         $manager->persist($product);
         $manager->flush();
