e16c009d61bff87acab53521778c91995a6a9394
Drakx116 <alexandre2.guyot@gmail.com>
2021-04-25 21:49:43 +0200

    Newsletter - Send confirmation email on registration success

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Entity\NewsletterUser;
 use App\Form\NewsletterType;
+use App\Services\Utils\Mailer;
 use App\Services\Managers\NewsletterUserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class NewsletterController extends AbstractController
 {
@@ -16,20 +20,29 @@ class NewsletterController extends AbstractController
      * @var NewsletterUserManager
      */
     private $newsletterUserManager;
+    /**
+     * @var Mailer
+     */
+    private $mailer;
 
     /**
      * NewsletterController constructor.
      * @param NewsletterUserManager $newsletterUserManager
+     * @param Mailer                $mailer
      */
-    public function __construct(NewsletterUserManager $newsletterUserManager)
+    public function __construct(NewsletterUserManager $newsletterUserManager, Mailer $mailer)
     {
         $this->newsletterUserManager = $newsletterUserManager;
+        $this->mailer = $mailer;
     }
 
     /**
-     * @Route("/newsletter", name="newsletter")
+     * @Route("/newsletter", name="app_newsletter")
      * @param Request $request
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function index(Request $request): Response
     {
@@ -41,7 +54,11 @@ class NewsletterController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $newUser = $form->getData();
             $newUser->setActive(true);
-            $this->newsletterUserManager->save($user);
+            $created = $this->newsletterUserManager->save($user);
+
+            if ($created) {
+                $this->mailer->sendNewsletterRegistrationEmail($user);
+            }
         }
 
         return $this->render('newsletter/index.twig', [
